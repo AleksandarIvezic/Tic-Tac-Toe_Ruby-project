9ea820a506914ab9e3ce206eb408d514060394ef
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-04-27 11:27:00 +0200

    Add code to main.rb for user_interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,27 @@
 #!/usr/bin/env ruby
-puts 'Hello World!'
+puts "Enter the player 1 name:"
+player1 = gets.chomp
+puts  "Enter the player 2 name:"
+player2 = gets.chomp
+
+puts "#{player1} will play with X and #{player2} will play with O"
+puts "Let start!"
+gameover = false;
+pl1_choise=[]
+pl2_choise=[]
+while !gameover do
+    puts "+---+---+---+ \n| 1 | 2 | 3 | \n+---+---+---+ \n| 4 | 5 | 6 | \n+---+---+---+ \n| 7 | 8 | 9 | \n+---+---+---+ "
+    
+    puts "It's #{player1} turn"
+    puts "Please select an available cell on the board"
+    pl1_choise.push(gets.chomp)
+    
+    puts "It's #{player2} turn"
+    puts "Please select an available cell on the board"
+    pl2_choise.push(gets.chomp)
+    
+    puts "Player  #{player1} win!"
+
+    gameover = true
+
+end
