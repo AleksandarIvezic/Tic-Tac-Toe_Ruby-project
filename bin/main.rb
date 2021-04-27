e353fcc09aa18a2136084599f950bde0b47859d0
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-04-27 17:32:12 +0200

    Fix robocup errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 
-require "./lib/logic.rb"
+require './lib/logic'
 
 puts 'Enter the player 1 name:'
-player1 = Player.new(gets.chomp, "X")
+player1 = Player.new(gets.chomp, 'X')
 puts 'Enter the player 2 name:'
-player2 = Player.new(gets.chomp, "O")
+player2 = Player.new(gets.chomp, 'O')
 
 puts "#{player1.name} will play with X and #{player2.name} will play with O"
 puts 'Let start!'
@@ -19,21 +19,21 @@ until gameover
   puts "It's #{player1.name} turn"
   puts 'Please select an available cell on the board'
   begin
-    cell = gets.chomp.match(/[1-9]/)[0] 
+    cell = gets.chomp.match(/[1-9]/)[0]
     gaming.choice_available?(cell.to_i)
   rescue StandardError
     puts 'Enter valid number'
     retry
   else
-    gaming.turn(player1,cell.to_i)
+    gaming.turn(player1, cell.to_i)
     if player1.choices.length == 5
-    puts "It's a draw!" 
-    gameover = true
-    next
+      puts "It's a draw!"
+      gameover = true
+      next
     elsif player1.check_winner
-        puts "Player  #{player1.name} win!"
-        gameover = true
-        next
+      puts "Player  #{player1.name} win!"
+      gameover = true
+      next
     end
   end
   gaming.display_board
@@ -46,17 +46,13 @@ until gameover
     puts 'Enter valid number'
     retry
   else
-    gaming.turn(player2,cell.to_i)
+    gaming.turn(player2, cell.to_i)
     if player2.check_winner
-        puts "Player  #{player2.name} win!"
-        gameover = true
-        next
+      puts "Player  #{player2.name} win!"
+      gameover = true
+      next
     end
   end
-
-  
-
-  
 
 end
 
--- lib/logic.rb
+++ lib/logic.rb
@@ -1,53 +1,59 @@
 class Game
-  def initialize (player1,player2)
+  def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     @board = []
-    9.times {@board.push(Cell.new)}
+    9.times { @board.push(Cell.new) }
   end
 
-  def turn (player,choice)
+  def turn(player, choice)
     player.play(choice)
-    @board[choice-1].chosen = true
-    @board[choice-1].sym = player.symbol
+    @board[choice - 1].chosen = true
+    @board[choice - 1].sym = player.symbol
   end
 
-  def choice_available? (choice)
-   raise StandardError if @board[choice-1].chosen
+  def choice_available?(choice)
+    raise StandardError if @board[choice - 1].chosen
   end
 
-  def display_board 
-    puts "+---+---+---+ \n| #{@board[0].sym} | #{@board[1].sym} | #{@board[2].sym} | \n+---+---+---+ \n| #{@board[3].sym} | #{@board[4].sym} | #{@board[5].sym} | \n+---+---+---+ \n| #{@board[6].sym} | #{@board[7].sym} | #{@board[8].sym} | \n+---+---+---+ "
+  def display_board
+    puts '+---+---+---+ '
+    puts "| #{@board[0].sym} | #{@board[1].sym} | #{@board[2].sym} | "
+    puts "+---+---+---+" 
+    puts "| #{@board[3].sym} | #{@board[4].sym} | #{@board[5].sym} | "
+    puts "+---+---+---+" 
+    puts "| #{@board[6].sym} | #{@board[7].sym} | #{@board[8].sym} | "
+    puts '+---+---+---+ '
   end
-
 end
 
 class Player
-    WINNING = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]] 
-    attr_reader :name , :symbol, :choices
-    def initialize(name, symbol)
-      @name = name
-      @choices = []
-      @symbol = symbol
-    end
+  WINNING = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]].freeze
+  attr_reader :name, :symbol, :choices
 
-    def check_winner 
-        WINNING.each {|win| return true if (win - @choices).empty? } 
-        false
-    end
-
-    def play(choice)
-      @choices.push(choice) 
-    end
+  def initialize(name, symbol)
+    @name = name
+    @choices = []
+    @symbol = symbol
   end
-  
+
+  def check_winner
+    WINNING.each { |win| return true if (win - @choices).empty? }
+    false
+  end
+
+  def play(choice)
+    @choices.push(choice)
+  end
+end
+
 class Cell
-    @@id = 0
-    attr_accessor :sym
-    attr_accessor :chosen
-    def initialize
-        @@id += 1
-        @chosen = false
-        @sym = "#{@@id}"
-    end
+  @@id = 0
+  attr_accessor :sym, :chosen
+
+  def initialize
+    @@id += 1
+    @chosen = false
+    @sym = @@id.to_s
+  end
 end
