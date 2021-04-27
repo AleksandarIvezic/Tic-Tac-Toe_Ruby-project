824fe4c4a787c382e96a6b9974662bb0773b78f9
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-04-27 16:03:27 +0200

    Add Game, Player and Cell classes to logic.rb and connect with main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,37 +1,43 @@
 #!/usr/bin/env ruby
-puts 'Enter the player 1 name:'
-player1 = gets.chomp
-puts 'Enter the player 2 name:'
-player2 = gets.chomp
 
-puts "#{player1} will play with X and #{player2} will play with O"
+require "./lib/logic.rb"
+
+puts 'Enter the player 1 name:'
+player1 = Player.new(gets.chomp, "X")
+puts 'Enter the player 2 name:'
+player2 = Player.new(gets.chomp, "O")
+
+puts "#{player1.name} will play with X and #{player2.name} will play with O"
 puts 'Let start!'
+
+gaming = Game.new(player1, player2)
 gameover = false
-pl1_choise = []
-pl2_choise = []
+
 until gameover
   puts "+---+---+---+ \n| 1 | 2 | 3 | \n+---+---+---+ \n| 4 | 5 | 6 | \n+---+---+---+ \n| 7 | 8 | 9 | \n+---+---+---+ "
 
-  puts "It's #{player1} turn"
+  puts "It's #{player1.name} turn"
   puts 'Please select an available cell on the board'
   begin
-    cell = gets.chomp.match(/[1-9]/)[0]
+    cell = gets.chomp.match(/[1-9]/)[0] 
+    gaming.choice_available?(cell.to_i)
   rescue StandardError
     puts 'Enter valid number'
     retry
   else
-    pl1_choise.push(cell)
+    gaming.turn(player1,cell.to_i)
   end
 
-  puts "It's #{player2} turn"
+  puts "It's #{player2.name} turn"
   puts 'Please select an available cell on the board'
   begin
     cell = gets.chomp.match(/[1-9]/)[0]
+    gaming.choice_available?(cell.to_i)
   rescue StandardError
     puts 'Enter valid number'
     retry
   else
-    pl2_choise.push(cell)
+    gaming.turn(player2,cell.to_i)
   end
 
   puts "Player  #{player1} win!"
--- lib/logic.rb
+++ lib/logic.rb
@@ -1,15 +1,40 @@
+class Game 
+  def initialize (player1,player2)
+    @player1 = player1
+    @player2 = player2
+    @board = []
+    9.times {@board.push(Cell.new)}
+  end
+
+  def turn (player,choice)
+    player.play(choice)
+    @board[choice-1].chosen = true
+  end
+
+  def choice_available? (choice)
+   
+   raise StandardError if @board[choice-1].chosen
+  end
+
+end
+
 class Player
-    def initialize(name)
+    attr_reader :name
+    def initialize(name, symbol)
       @name = name
       @choices = []
+      @symbol = symbol
     end
   
-    def play(choice); end
+    def play(choice)
+      @choices.push(choice) 
+    end
   end
   
   class Cell
     @@id = 0
-  
+    attr_accessor :sym
+    attr_accessor :chosen
     def initialize
       @@id += 1
       @chosen = false
