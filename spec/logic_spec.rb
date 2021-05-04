5bc2695aa778b675f39fc51f8ed10ca62572829a
Barack <barackmukelenga100@gmail.com>
2021-05-04 10:40:58 +0200

    Add test for the case where we don't pass any arguments to the class

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -6,10 +6,22 @@ describe Game do
         let(:player1) {"Aleksandar"}
         let(:player2) {"Baraka"}
        context "when player1 and player2 are specified" do
-           it "create variables player1 and player2" do
+           it "create variable player1" do
             game = Game.new(player1,player2)
                expect(game.player1).to eql("Aleksandar")
            end
+           it "create variable player2" do
+            game = Game.new(player1,player2)
+               expect(game.player2).to eql("Baraka")
+           end
        end
+
+       context "when player1 and player2 are not specified" do
+        it "raise error" do
+            expect{Game.new}.to raise_error
+        end
+    end
    end
+
+   
 end
