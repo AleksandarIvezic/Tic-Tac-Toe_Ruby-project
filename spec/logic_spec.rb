71fde383549e73a9fa98362065366787e4b39453
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-05-04 11:40:15 +0200

    Add tests for turn method of Game class

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -21,6 +21,29 @@ describe Game do
             expect{Game.new}.to raise_error
         end
     end
+
+
+    describe "#turn" do
+           
+         context "when player make it's turn" do  
+            let (:game) {Game.new("Baraka","p2")} 
+            let (:choice) {5}
+            let (:player) {Player.new("Baraka", "X")}
+            
+            it "player choose is true" do
+                game.turn(player,choice) 
+                expect(game.board[choice-1].chosen).to be true
+            end
+            it "board cell get player symbol" do
+                game.turn(player,choice) 
+                expect(game.board[choice-1].sym).to eql(player.symbol)
+            end
+            it "check player choices" do
+                game.turn(player,choice) 
+                expect(player.choices).to include choice
+            end
+        end
+    end
    end
 
    
