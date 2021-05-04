4776e6c5da1cdca22b50a76f33a0f7ddeca679f4
Barack <barackmukelenga100@gmail.com>
2021-05-04 14:17:39 +0200

    Add tests for the Cell class

--- lib/cell.rb
+++ lib/cell.rb
@@ -2,11 +2,14 @@ class Cell
   # rubocop: disable Metrics/ClassVars
   @@id = 0
   attr_accessor :sym, :chosen
-
+  
   def initialize
     @@id += 1
     @chosen = false
     @sym = @@id.to_s
+  end
+  def new_id
+    @@id 
   end
   # rubocop:enable Metrics/ClassVars
 end
---
+++ spec/cell_spec.rb
@@ -0,0 +1,21 @@
+require "rspec"
+require_relative "../lib/cell.rb"
+
+RSpec.describe Cell do 
+    describe "#initialize" do 
+        it "have the variable id incremented" do 
+            cell = Cell.new
+            expect(cell.new_id).to eql(1)
+        end
+
+        it "have the variable chosen equals false" do 
+            cell = Cell.new
+            expect(cell.chosen).to be false
+        end
+
+        it "have the variable sym equals to id" do 
+            cell = Cell.new
+            expect(cell.sym).to eql(3.to_s)
+        end
+    end
+end
