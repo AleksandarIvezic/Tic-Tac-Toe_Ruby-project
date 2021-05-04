79f5dfba848a1f5f1a2a52c34688fa2fccf8072e
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-05-04 14:36:56 +0200

    Fix rubocop errors

--- lib/cell.rb
+++ lib/cell.rb
@@ -2,14 +2,15 @@ class Cell
   # rubocop: disable Metrics/ClassVars
   @@id = 0
   attr_accessor :sym, :chosen
-  
+
   def initialize
     @@id += 1
     @chosen = false
     @sym = @@id.to_s
   end
+
   def new_id
-    @@id 
+    @@id
   end
   # rubocop:enable Metrics/ClassVars
 end
